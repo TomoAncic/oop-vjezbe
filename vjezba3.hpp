e1472992b873c7129463a3cfa96e59683a1d62e0
TomoAncic <148474191+TomoAncic@users.noreply.github.com>
2023-11-02 13:56:42 +0100

    Update vjezba3.hpp

--- vjezba3.hpp
+++ vjezba3.hpp
@@ -7,5 +7,8 @@ using namespace std;
 void unos1(vector<int>& v, int velicina);
 void unos2(vector<int>& v, int d, int g);
 void ispis(vector<int>& v);
+vector<int> drugizad(vector<int>& prvi, vector<int>& drugi);
+void zadatak3(vector<int>& zad3);
+void zadatak4(vector<int>& v, int element);
 
 #endif
