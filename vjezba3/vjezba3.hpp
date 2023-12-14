b0a03190d8ca335b3c7ee958e860a7a4557863d1
TomoAncic <148474191+TomoAncic@users.noreply.github.com>
2023-12-14 13:53:48 +0100

    Create vjezba3.hpp

---
+++ vjezba3/vjezba3.hpp
@@ -0,0 +1,14 @@
+#ifndef VJEZBA3_HPP
+#define VJEZBA3_HPP
+#include <vector>
+#include <iostream>
+using namespace std;
+
+void unos1(vector<int>& v, int velicina);
+void unos2(vector<int>& v, int d, int g);
+void ispis(vector<int>& v);
+vector<int> drugizad(vector<int>& prvi, vector<int>& drugi);
+void zadatak3(vector<int>& zad3);
+void zadatak4(vector<int>& v, int element);
+
+#endif
