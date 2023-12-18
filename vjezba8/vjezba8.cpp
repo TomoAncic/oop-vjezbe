f8ba61220d90009be9cd921be144def9841ea74b
TomoAncic <148474191+TomoAncic@users.noreply.github.com>
2023-12-18 15:20:50 +0100

    Update vjezba8.cpp

--- vjezba8/vjezba8.cpp
+++ vjezba8/vjezba8.cpp
@@ -12,14 +12,24 @@ int main()
 	{
 		string ime = "Martin";
 		string ime2 = "Ivan";
-		string oruzje = "";
+		string oruzje = "mac";
 		string sposobnost = "Fire ball";
 		neprijatelji.push_back(new Boss(ime, 100, 50, oruzje));
 		neprijatelji.push_back(new Monster(ime2, 70, 20, sposobnost));
 
 	}
-	catch (const std::invalid_argument err)
+	catch (const std::invalid_argument& err)
 	{
-		cout << "Error"<<err.what();
+		cout << "Error" << err.what() << endl;
+	}
+
+	for (auto& neprijatelj : neprijatelji)
+	{
+		if (neprijatelj)
+		{
+			neprijatelj->attack();
+			neprijatelj->displayInfo();
+			delete neprijatelj;
+		}
 	}
 }
