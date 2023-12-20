1ebb054e0a5b50e1eaed35028fb06e84660a9bb5
TomoAncic <148474191+TomoAncic@users.noreply.github.com>
2023-12-20 20:53:41 +0100

    Update vjezba7-main.cpp

--- vjezba7/vjezba7-main.cpp
+++ vjezba7/vjezba7-main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include "vjezba7.hpp"
 using namespace std;
 
 int main()
 {
-	
+	Food food;
+	food.set_counter(10);
+	cout << "Trenutno stanje porcija: " << get_counter(food) << endl;
+
+	food.povecaj_porcije(2);
+	cout << "Nakon povecana porcija stanje je: " << get_counter(food) << endl;
+
+	food.smanji_porcije(4);
+	cout << "Nakon smanjenja porcija stanje je: " << get_counter(food) << endl;
 }
