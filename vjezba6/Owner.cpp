6240c2898ee4a542aea699ab4e2154e3a533b69f
TomoAncic <148474191+TomoAncic@users.noreply.github.com>
2023-12-20 20:33:37 +0100

    Create Owner.cpp

---
+++ vjezba6/Owner.cpp
@@ -0,0 +1,54 @@
+#include "Owner.hpp"
+#include <iostream>
+#include <algorithm>
+#include <random>
+using namespace std;
+
+Owner::Owner(const string& ime)
+{
+    this->ime = ime;
+}
+
+void Owner::dodaj_ljubimca(VirtualPet ljubimac) {
+    ljubimci.push_back(ljubimac);
+}
+
+void Owner::radnja() {
+   
+    srand(static_cast<unsigned int>(time(0)));
+    for (auto& ljubimac : ljubimci) {
+        int a = rand() % 3;
+
+        switch (a) {
+        case 1:
+            ljubimac.igraj_se();
+            break;
+        case 2:
+            ljubimac.jedi();
+            break;
+        case 3:
+            ljubimac.spavaj();
+            break;
+        }
+    }
+}
+
+std::string& Owner::return_ime(){
+    return ime;
+}
+
+std::vector<VirtualPet>& Owner::return_ljubimci(){
+    return ljubimci;
+}
+
+VirtualPet Owner::najsretniji() {
+    VirtualPet najsretniji = ljubimci[0];
+    for (auto& ljubimac : ljubimci)
+    {
+        if (ljubimac.return_sreca() > najsretniji.return_sreca())
+        {
+            najsretniji = ljubimac;
+        }
+    }
+    return najsretniji;
+}
